251bf4750337f1b4318abc96e7ad28f0feb0478e
Odin <noxtrad77@gmail.com>
2023-01-17 20:17:36 +0100

    game system base added

--- js/simon_game.js
+++ js/simon_game.js
@@ -3,18 +3,56 @@ const simonGame = {
     nbRow: 3,
     nbCell: 3,
     launcherButton: null,
-    rowElements: [],
+    userChoice: 0,
     cellElements: [],
+    cell_in_memory: [],
     game: function() {
         console.log("Game launched");
-        simonGame.status = "";
+        let randomElement = simonGame.randomCell();
+        simonGame.cell_in_memory.push(randomElement);
         
+        let index = 0;
+        let id = setInterval(function() {
+            if (index === simonGame.cell_in_memory.length) {
+                clearInterval(id);
+                simonGame.status = "";
+            } else {
+                let element = simonGame.cell_in_memory[index];
+                simonGame.cellActive(element, 400);
+                index++;
+            }
+        }, 500)
+        // simonGame.cellActive(randomElement, 300);
+        
+        
+    },
+    randomCell: function() {
+        let randomValue = Math.floor(Math.random() * simonGame.cellElements.length);
+        return simonGame.cellElements[randomValue];
     },
     playerListener: function(event) {
         if (simonGame.status === "ordi") {
             return;
         }
-        simonGame.cellActive(event.currentTarget, 100);
+
+        
+        let element = simonGame.cell_in_memory[simonGame.userChoice];
+        
+        if (event.currentTarget !== element) {
+            alert("Désolé c'est perdu");
+            return;
+        }
+        simonGame.cellActive(event.currentTarget, 200);
+        simonGame.userChoice += 1;
+
+        if (simonGame.userChoice === simonGame.cell_in_memory.length) {
+            simonGame.status = "ordi";
+            simonGame.userChoice = 0;
+            setTimeout(function() {
+                simonGame.game();
+            }, 200);
+            
+        } 
     },
     cellActive: function(element, duration) {
         element.classList.add("active");
@@ -38,20 +76,20 @@ const simonGame = {
     },
     drawBoard: function() {
         let boardElement = document.querySelector(".game__board");
-
+        let divRowElements = []
         for (let i=0; i<simonGame.nbRow; i++) {
             let rowElement = document.createElement("div");
             rowElement.classList.add("game__board_row");
             boardElement.append(rowElement);
-            simonGame.rowElements.push(rowElement);
+            divRowElements.push(rowElement);
         }
 
-        simonGame.rowElements.forEach(function(element) {
+        divRowElements.forEach(function(element) {
             for (let i=0; i<simonGame.nbCell; i++) {
                 let cellElement = document.createElement("div");
                 cellElement.classList.add("game__board_cell");
                 cellElement.addEventListener("click", simonGame.playerListener);
-                simonGame.cellElements.push(element);
+                simonGame.cellElements.push(cellElement);
                 element.append(cellElement);
             }
         });
